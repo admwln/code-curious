e17169227b3430cde8c418d2ff1adbac1b4602c5
Adam Welin <143414967+admwln@users.noreply.github.com>
2024-10-18 15:24:50 +0200

    Move common tut layout from page to layout file

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -1,117 +1,28 @@
 <script lang="ts">
-	import { currentPanel } from '$lib/store';
-	import { isRunning } from '$lib/store';
-
-	import {
-		faChalkboardUser,
-		faCode,
-		faDesktop,
-		faFilter,
-		faMinus,
-		faPause,
-		faPlay,
-		faPlus,
-	} from '@fortawesome/free-solid-svg-icons';
+	import { faPlus } from '@fortawesome/free-solid-svg-icons';
 	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
-
-	import Matter from '../../components/Matter.svelte';
-	// Modified skeleton components
-	import Accordion from '../../components/skeleton/Accordion.svelte';
-	import AccordionItem from '../../components/skeleton/AccordionItem.svelte';
-
-	// Keep track of if the simulation is running
-	function toggleRun() {
-		$isRunning = !$isRunning;
-	}
 </script>
 
-<!-- Panel 1: Tutorial -->
-<div
-	class="bg-neutral-900 md:mr-0.5 lg:mx-0 w-full h-screen md:w-1/2 lg:w-1/3 lg:block {$currentPanel !==
-	1
-		? 'hidden'
-		: ''} overflow-y-scroll"
->
-	<h2 class="text-start w-full flex items-center gap-4 space-x-4 py-3 px-4 bg-[#ec489a2A]">
-		<FontAwesomeIcon icon={faChalkboardUser} /> Tutorial
-	</h2>
-	<!-- Dynamic content start -->
-	<h3 class="p-4">The Basics</h3>
-	<ul class="p-4 list">
-		<li>Data types</li>
-		<li>Variables</li>
-		<li>Operators</li>
-		<li>Functions</li>
-		<li>Loops</li>
-	</ul>
-	<p class="p-4">An array is a special variable, which can hold more than one value:</p>
-	<code class="p-4 block">["apple", "banana", "orange"]</code>
-	<p class="p-4">
-		You access an array element by referring to the index number. This statement accesses the value
-		of the first element in fruits.
-	</p>
-	<!-- Dynamic content end -->
-</div>
-
-<!-- Panel 2: Editor & Console -->
-<div
-	class="bg-neutral-900 md:mr-0.5 lg:mx-0.5 w-full h-screen md:w-1/2 lg:w-1/3 lg:block {$currentPanel !==
-	2
-		? 'hidden'
-		: ''} overflow-y-scroll"
->
-	<Accordion>
-		<AccordionItem open>
-			<svelte:fragment slot="iconClosed"><FontAwesomeIcon icon={faMinus} /></svelte:fragment>
-			<svelte:fragment slot="iconOpen"><FontAwesomeIcon icon={faPlus} /></svelte:fragment>
-			<svelte:fragment slot="lead"><FontAwesomeIcon icon={faCode} /></svelte:fragment>
-			<svelte:fragment slot="summary">
-				<div class="flex justify-between">
-					<h2>Editor</h2>
-				</div>
-			</svelte:fragment>
-			<svelte:fragment slot="content">
-				<!-- Dynamic content start -->
-				Here you will find all the code blocks!
-				<!-- Dynamic content end -->
-			</svelte:fragment>
-		</AccordionItem>
-		<AccordionItem open>
-			<svelte:fragment slot="iconClosed"><FontAwesomeIcon icon={faMinus} /></svelte:fragment>
-			<svelte:fragment slot="iconOpen"><FontAwesomeIcon icon={faPlus} /></svelte:fragment>
-			<svelte:fragment slot="lead"><FontAwesomeIcon icon={faDesktop} /></svelte:fragment>
-			<svelte:fragment slot="summary"><h2>Console</h2></svelte:fragment>
-			<svelte:fragment slot="content">
-				<!-- Dynamic content start -->
-				<div class="rounded-md bg-slate-900 p-4">
-					<code class="block">Hello World!</code>
-				</div>
-				<!-- Dynamic content end -->
-			</svelte:fragment>
-		</AccordionItem>
-	</Accordion>
-</div>
-
-<!-- Panel 3: Funnel (matter.js) -->
-<div
-	class="bg-neutral-900 w-full h-screen md:w-1/2 lg:w-1/3 lg:block {$currentPanel !== 3
-		? 'hidden md:block lg:block'
-		: ''} overflow-y-scroll"
->
-	<div
-		class="text-start w-full flex items-center justify-between space-x-4 py-2 px-4 bg-[#ec489a2a]"
-	>
-		<h2 class="flex items-center gap-4"><FontAwesomeIcon icon={faFilter} /> Funnel</h2>
-		<button type="button" on:click={toggleRun} class="btn btn-sm bg-primary-900 flex gap-2">
-			{#if $isRunning}
-				<FontAwesomeIcon icon={faPause} /> Running
-			{/if}
-			{#if !$isRunning}
-				<FontAwesomeIcon icon={faPlay} /> Run
-			{/if}
-		</button>
-	</div>
-	<!-- Dynamic content start -->
-	<Matter />
-	<!-- Dynamic content end -->
-</div>
+<section>
+	<a class="card card-hover overflow-hidden" href="/tutorial/welcome-to-funnel">
+		<header></header>
+		<div class="p-4 space-y-4">
+			<h6 class="h6" data-toc-ignore>Nothing to see here</h6>
+			<h3 class="h3" data-toc-ignore>Looks like you're lost!</h3>
+			<article>
+				<p>
+					The tutorial starts over at the <a class="anchor" href="/tutorial/welcome-to-funnel"
+						>Welcome to Funnel</a
+					> page.
+				</p>
+			</article>
+		</div>
+		<hr class="opacity-50" />
+		<footer class="p-4 flex justify-start items-center space-x-4">
+			<div class="flex-auto flex justify-between items-center">
+				<h6 class="font-bold" data-toc-ignore>By Alex</h6>
+				<small>On {new Date().toLocaleDateString()}</small>
+			</div>
+		</footer>
+	</a>
+</section>
--- src/routes/tutorial/[lessonId]/+page.svelte
+++ src/routes/tutorial/[lessonId]/+page.svelte
@@ -1,9 +1,73 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
-	import { page } from '$app/stores'; // Correct import for page store
+	import { currentPanel } from '$lib/store';
+	import { isRunning } from '$lib/store';
+	import { page } from '$app/stores'; // For dynamic routing
+	import { onDestroy } from 'svelte';
+
+	import {
+		faChalkboardUser,
+		faCode,
+		faDesktop,
+		faFilter,
+		faMinus,
+		faPause,
+		faPlay,
+		faPlus,
+	} from '@fortawesome/free-solid-svg-icons';
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+
+	// Import components
+	import Matter from '../../../components/Matter.svelte';
+
+	// Modified skeleton components
+	import Accordion from '../../../components/skeleton/Accordion.svelte';
+	import AccordionItem from '../../../components/skeleton/AccordionItem.svelte';
+
+	// Keep track of if the simulation is running
+	function toggleRun() {
+		$isRunning = !$isRunning;
+	}
+
+	interface LessonData {
+		tutorial: {
+			title: string;
+			description: string;
+			topics: { name: string }[];
+			exampleCode: string;
+		};
+		editor: {
+			defaultCode: string;
+		};
+		console: {
+			defaultCode: string;
+		};
+		funnel: {
+			color: string;
+		};
+	}
+
+	let lessonData: LessonData | null = null;
+	let lessonId = '';
 
 	// Example of locally stored lessons data
 	const lessons: { [key: string]: LessonData } = {
+		'welcome-to-funnel': {
+			tutorial: {
+				title: 'Welcome to Funnel Logic!',
+				description: 'This is the starting point of the tutorial.',
+				topics: [{ name: 'Introduction' }, { name: 'Getting started' }],
+				exampleCode: 'console.log("Hello, World!");',
+			},
+			editor: {
+				defaultCode: 'Here you will find all the code blocks!',
+			},
+			console: {
+				defaultCode: "console.log('Hello, World!');",
+			},
+			funnel: {
+				color: 'hotpink',
+			},
+		},
 		'lesson-1': {
 			tutorial: {
 				title: 'Lesson 1: Introduction to Variables',
@@ -16,7 +80,13 @@
 				exampleCode: 'let x = 5;\nlet y = 10;\nconsole.log(x + y);',
 			},
 			editor: {
+				defaultCode: 'Here you will find all the code blocks!',
+			},
+			console: {
 				defaultCode: "console.log('Hello, World!');",
+			},
+			funnel: {
+				color: 'seagreen',
 			},
 		},
 		'lesson-2': {
@@ -31,29 +101,20 @@
 				exampleCode: 'function add(x, y) {\n\treturn x + y;\n}\nconsole.log(add(5, 10));',
 			},
 			editor: {
+				defaultCode: 'Here you will find all the code blocks!',
+			},
+			console: {
 				defaultCode: "console.log('Hello, World!');",
+			},
+			funnel: {
+				color: 'gold',
 			},
 		},
 	};
 
-	interface LessonData {
-		tutorial: {
-			title: string;
-			description: string;
-			topics: { name: string }[];
-			exampleCode: string;
-		};
-		editor: {
-			defaultCode: string;
-		};
-	}
-
-	let lessonData: LessonData | null = null;
-	let lessonId = '';
-
 	// Subscribe to the `page` store to get the current route parameters
 	const unsubscribe = page.subscribe(({ params }) => {
-		lessonId = params.lessonId; // Assuming `lessonId` is a dynamic route param
+		lessonId = params.lessonId; // `lessonId` is a dynamic route param
 		lessonData = lessons[lessonId] || null; // Fetch the lesson data or set to null if not found
 	});
 
@@ -63,15 +124,97 @@
 	});
 </script>
 
-{#if lessonData}
-	<h1>{lessonData.tutorial.title}</h1>
-	<p>{lessonData.tutorial.description}</p>
-	<ul>
-		{#each lessonData.tutorial.topics as topic}
-			<li>{topic.name}</li>
-		{/each}
-	</ul>
-	<code>{lessonData.tutorial.exampleCode}</code>
-{:else}
-	<p>Loading...</p>
-{/if}
+<!-- Panel 1: Tutorial -->
+<section
+	class="bg-neutral-900 md:mr-0.5 lg:mx-0 w-full h-screen md:w-1/2 lg:w-1/3 lg:block {$currentPanel !==
+	1
+		? 'hidden'
+		: ''} overflow-y-scroll"
+>
+	<h2 class="text-start w-full flex items-center gap-4 space-x-4 py-3 px-4 bg-[#ec489a2A]">
+		<FontAwesomeIcon icon={faChalkboardUser} /> Tutorial
+	</h2>
+	<!-- Dynamic content start -->
+	<div class="p-4">
+		{#if lessonData}
+			<h1>{lessonData.tutorial.title}</h1>
+			<p>{lessonData.tutorial.description}</p>
+			<ul>
+				{#each lessonData.tutorial.topics as topic}
+					<li>{topic.name}</li>
+				{/each}
+			</ul>
+			<code>{lessonData.tutorial.exampleCode}</code>
+		{:else}
+			<p>Loading...</p>
+		{/if}
+	</div>
+	<!-- Dynamic content end -->
+</section>
+
+<!-- Panel 2: Editor & Console -->
+<section
+	class="bg-neutral-900 md:mr-0.5 lg:mx-0.5 w-full h-screen md:w-1/2 lg:w-1/3 lg:block {$currentPanel !==
+	2
+		? 'hidden'
+		: ''} overflow-y-scroll"
+>
+	<Accordion>
+		<AccordionItem open>
+			<svelte:fragment slot="iconClosed"><FontAwesomeIcon icon={faMinus} /></svelte:fragment>
+			<svelte:fragment slot="iconOpen"><FontAwesomeIcon icon={faPlus} /></svelte:fragment>
+			<svelte:fragment slot="lead"><FontAwesomeIcon icon={faCode} /></svelte:fragment>
+			<svelte:fragment slot="summary">
+				<div class="flex justify-between">
+					<h2>Editor</h2>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="content">
+				<!-- Dynamic content start -->
+				{#if lessonData}
+					<code>{lessonData.editor.defaultCode}</code>
+				{/if}
+				<!-- Dynamic content end -->
+			</svelte:fragment>
+		</AccordionItem>
+		<AccordionItem open>
+			<svelte:fragment slot="iconClosed"><FontAwesomeIcon icon={faMinus} /></svelte:fragment>
+			<svelte:fragment slot="iconOpen"><FontAwesomeIcon icon={faPlus} /></svelte:fragment>
+			<svelte:fragment slot="lead"><FontAwesomeIcon icon={faDesktop} /></svelte:fragment>
+			<svelte:fragment slot="summary"><h2>Console</h2></svelte:fragment>
+			<svelte:fragment slot="content">
+				<!-- Dynamic content start -->
+				<div class="rounded-md bg-slate-800 p-4">
+					{#if lessonData}
+						<code>{lessonData.console.defaultCode}</code>
+					{/if}
+				</div>
+				<!-- Dynamic content end -->
+			</svelte:fragment>
+		</AccordionItem>
+	</Accordion>
+</section>
+
+<!-- Panel 3: Funnel (matter.js) -->
+<section
+	class="bg-neutral-900 w-full h-screen md:w-1/2 lg:w-1/3 lg:block {$currentPanel !== 3
+		? 'hidden md:block lg:block'
+		: ''} overflow-y-scroll"
+>
+	<div
+		class="text-start w-full flex items-center justify-between space-x-4 py-2 px-4 bg-[#ec489a2a]"
+	>
+		<h2 class="flex items-center gap-4"><FontAwesomeIcon icon={faFilter} /> Funnel</h2>
+		<button type="button" on:click={toggleRun} class="btn btn-sm bg-primary-900 flex gap-2">
+			{#if $isRunning}
+				<FontAwesomeIcon icon={faPause} /> Running
+			{/if}
+			{#if !$isRunning}
+				<FontAwesomeIcon icon={faPlay} /> Run
+			{/if}
+		</button>
+	</div>
+	<!-- Dynamic content start -->
+	<Matter />
+	<!-- Dynamic content end -->
+</section>
